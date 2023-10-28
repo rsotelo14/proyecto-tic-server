fa2d451a2972f73c321668c610f6d6a2c016b7b1
rodrigo sotelo <rsotelosilva@gmail.com>
2023-10-28 00:37:34 -0300

    corregir errores merge 2

--- src/main/java/uy/um/edu/server/business/managers/AvionMgr.java
+++ src/main/java/uy/um/edu/server/business/managers/AvionMgr.java
@@ -2,6 +2,7 @@ package uy.um.edu.server.business.managers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uy.um.edu.server.business.entities.aerolinea.Aerolinea;
 import uy.um.edu.server.business.entities.vuelos.Avion;
 import uy.um.edu.server.business.exceptions.EntidadYaExiste;
 import uy.um.edu.server.persistence.vuelos.AvionRepository;
@@ -12,12 +13,20 @@ import java.util.List;
 public class AvionMgr {
     @Autowired
     private AvionRepository avionRepository;
+
+    @Autowired
+    private AerolineaMgr aerolineaMgr;
     public Avion obtenerUnoPorCodigo(String codigoAvion) {
         return avionRepository.findOneByCodigo(codigoAvion);
     }
     public void agregarAvion(Avion avion) throws EntidadYaExiste {
         if (avionRepository.findOneByCodigo(avion.getCodigo()) != null)
             throw new EntidadYaExiste("Avion ya existe");
+
+        Aerolinea aerolinea = aerolineaMgr.obtenerUnoPorCodigoIATA(avion.getAerolinea().getCodigoIATA());
+        assert aerolinea.equals(avion.getAerolinea());
+        avion.setAerolinea(aerolinea);
+
         avionRepository.save(avion);
 
     }
